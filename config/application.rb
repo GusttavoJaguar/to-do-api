881bfb5d695d523ac8e1c78f37ba0fc151b1e7ae
gusttavojaguar <gutumendes8@gmail.com>
2025-04-15 16:43:54 -0300

    Deploy do projeto no render - Correção da conexão com o banco de dados

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module TodoApi
 
     # Configuration for the application, engines, and railties goes here.
     #
+    config.active_record.adapter = :postgresql
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Instala TODAS as gems (incluindo development/test)
+bundle config set --local without ''
 bundle install
+
+# Configura SSL para PostgreSQL
+export PGSSLMODE=require
+
+# Migrações principais
+bundle exec rails db:migrate
+
+# Migrações do Solid Cache
+bundle exec rails solid_cache:install:migrations
+bundle exec rails db:migrate
+
+# Migrações do Solid Queue
+bundle exec rails solid_queue:install:migrations
 bundle exec rails db:migrate
