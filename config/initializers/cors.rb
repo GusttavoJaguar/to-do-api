0dcc1d8a49e640a2be487e3815b6e75d44d739b9
gusttavojaguar <gutumendes8@gmail.com>
2025-04-15 18:49:03 -0300

    Deploy do projeto no render - Correção da conexão com o banco de API axios

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-      origins "http://localhost:3000"
+      origins "https://to-do-frontend-iztg.onrender.com/"
  
       resource "*",
         headers: :any,
