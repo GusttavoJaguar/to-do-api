b7a40c85d6a4b4375653c4efd820de87b35322fc
gusttavojaguar <gutumendes8@gmail.com>
2025-04-15 23:05:47 -0300

    Deploy do projeto no render - Correção da conexão com o banco de dados

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,8 @@ module TodoApi
 
     config.middleware.use Rack::Cors do
       allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        origins 'https://to-do-api-nt50.onrender.com/'
+        resource 'https://to-do-frontend-iztg.onrender.com/', headers: :any, methods: [:get, :post, :options]
       end
     end
 
