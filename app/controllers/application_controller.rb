bbaf5c623673232bfafb185544f18d08500afe75
gusttavojaguar <gutumendes8@gmail.com>
2025-04-15 22:39:11 -0300

    Deploy do projeto no render - Correção da conexão com o banco de dados

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
-class ApplicationController < ActionController::API
-end
+class ApplicationController < ActionController::API # Ou ActionController::Base
+    include ActionController::MimeResponds
+    respond_to :json
+    
+    before_action :cors_preflight_check
+    after_action :cors_set_access_control_headers
+  
+    def cors_set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = 'https://to-do-frontend-iztg.onrender.com'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept'
+      headers['Access-Control-Max-Age'] = '1728000'
+    end
+  
+    def cors_preflight_check
+      if request.method == 'OPTIONS'
+        headers['Access-Control-Allow-Origin'] = 'https://to-do-frontend-iztg.onrender.com'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        render plain: '', content_type: 'text/plain'
+      end
+    end
+  end
